a66f165fa9ad07717f724c8ec109b6a177fe83bd
Fedor aka MXkodo <nemchinow2018@mail.ru>
2024-03-24 21:11:43 +0400

    Fix script

--- precode.sh
+++ precode.sh
@@ -6,11 +6,12 @@
 #   dir2
 #   dir3
 #       dir4
+mkdir -p task/dir1
+mkdir -p task/dir2
 mkdir -p task/dir3/dir4
 # изменяем текущую директорию на task
 cd task
 # создаём пустой файл task/dir2/empty
-mkdir -p dir2
 touch dir2/empty
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
@@ -25,12 +26,11 @@ ls dir2 > dir2/list.txt
 cp -r dir2/* dir3/dir4/
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
-mkdir -p dir1
 find . -type f -name "*.txt" > dir1/summary.txt
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 cat dir2/list.txt >> dir1/summary.txt
 # определяем переменную окружения NAME со значением "Всем студентам"
-export NAME="Всем студентам"
+NAME="Всем студентам"
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
 ./dir2/hello.sh "$NAME" >> dir1/summary.txt
